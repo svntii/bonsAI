ad9c1a1999ba5c4c35b2e3d160f1e7bcbc3a0269
Santiago Rodriguez <svntiiago@gmail.com>
2024-03-18 20:00:59 -0400

    init project

---
+++ brAIn/BHistorian.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+### Private
+
+class _BHistorianOutput:
+    pass
+
+
+
+
+### PUBLIC
+
+class BHistorian:
+
+    def __init__(self, name) -> None:
+        
+        self.name = name
+    
+
+    def userPrompt(self, prompt: str) -> any:
+        pass
+
+
+
+class BHistorianInput:
+    pass
+
+class BHistorianResponse:
+
+    def __init__(self, response: str):
+        
+        self.response = response
---
+++ brAIn/brAIn.py
@@ -0,0 +1,23 @@
+# brAIn.py
+
+
+from BHistorian import *
+from BVectorStore import *
+
+'''
+# TODO
+
+    -   define interface for historian
+    -   define interface for VectorStore
+    -   UI/UX
+
+'''
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
