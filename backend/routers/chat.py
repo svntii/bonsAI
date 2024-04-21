2dd26e29aee0d0eafceed94813b9154910809e3b
Ka Eun Kim <kkim24@nd.edu>
2024-04-21 14:41:20 -0400

    dummy backend api functionality for front end testing

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -2,18 +2,24 @@ from fastapi import APIRouter, Response, Body, status
 import jsonschema
 from backend.schema import PROMPT_SCHEMA
 from backend.agent import chain
+from typing import List
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/chat"
 )
 
+class ChatResponse(BaseModel):
+    id: int
+    responseText: str
+    userResponseSuggestions: List[str]
 
 '''
 Input looks like:
 POST
 {"prompt": "<user input>"}
 '''
-@router.post("")
+@router.post("", response_model=ChatResponse)
 def invoke_agent(response: Response, body: dict = Body(...)):
     try:
         jsonschema.validate(body, PROMPT_SCHEMA)
@@ -24,11 +30,29 @@ def invoke_agent(response: Response, body: dict = Body(...)):
     
     response = chain.invoke({"input": body["prompt"]})
 
-    print(response)
+    # print(response)
 
-    return {
-        "completion": response.content
-    }
+    # return {
+    #     "completion": response.content
+    # }
+    response_id = 1
+    response_text = response.content
+    suggestions = ["Successful initial response", "Suggestion 1", "Suggestion 2", "Suggestion 3"]
     
+    return ChatResponse(id=response_id, response_text=response_text, suggestions=suggestions)
 
+@router.post("/{id}", response_model=ChatResponse)
+def invoke_agent_with_id(response: Response, id: int, body: dict = Body(...)):
+    try:
+        jsonschema.validate(body, PROMPT_SCHEMA)
+    except jsonschema.ValidationError as error:
+        print(error)
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"message": error.message}
+    
+    response = chain.invoke({"input": body["prompt"]})
 
+    response_text = response.content
+    suggestions = ["Succesful response with id", "Suggestion 1", "Suggestion 2", "Suggestion 3"]
+    
+    return ChatResponse(id=id, responseText=response_text, userResponseSuggestions=suggestions)
