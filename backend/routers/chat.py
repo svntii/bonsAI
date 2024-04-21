75461f46a50a9d27e203410823007d7ad5611b04
lukvmil <millerluke1364@gmail.com>
2024-04-21 16:05:16 -0400

    added create conversation endpoint, saving conversation history for later recall

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -1,20 +1,33 @@
 from fastapi import APIRouter, Response, Body, status
 import jsonschema
+from uuid import uuid4
 from backend.schema import PROMPT_SCHEMA
 from backend.agent import chain
+from backend import conversation
 
 router = APIRouter(
     prefix="/chat"
 )
 
+@router.post("")
+def new_conversation(response: Response):
+    conversation_id = str(uuid4())
+
+    conversation.history[conversation_id] = []
+
+    print(conversation.history)
+
+    return {
+        "id": conversation_id
+    }
 
 '''
 Input looks like:
 POST
 {"prompt": "<user input>"}
 '''
-@router.post("")
-def invoke_agent(response: Response, body: dict = Body(...)):
+@router.post("/{conversation_id}")
+def invoke_agent(response: Response, conversation_id: str, body: dict = Body(...)):
     try:
         jsonschema.validate(body, PROMPT_SCHEMA)
     except jsonschema.ValidationError as error:
@@ -22,12 +35,29 @@ def invoke_agent(response: Response, body: dict = Body(...)):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": error.message}
     
-    response = chain.invoke({"input": body["prompt"]})
+    print(conversation_id)
+    print(conversation.history)
+    
+    history = conversation.history.get(conversation_id, None)
 
-    print(response)
+    if history is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "conversation id not found"}
+
+    user_prompt = body["prompt"]    
+    invocation = chain.invoke({"input": body["prompt"]})
+    bot_response = invocation["content"]
+
+    history.append({
+        "user": user_prompt,
+        "bot": bot_response
+    })
+
+    print(bot_response)
+
 
     return {
-        "completion": response.content
+        "completion": bot_response
     }
     
 
